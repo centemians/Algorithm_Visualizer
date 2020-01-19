497ba0e104d8853b6a6cd03d294041ddec2f728a
Abhishek kumar shakya <shakya.abhishek22@gmail.com>
2020-01-20 00:37:04 +0530

    Completed project

---
+++ README.md
@@ -0,0 +1,10 @@
+### Project Description
+
+The project is meant to visualize the different sorting Algorithms, and to know how these algorithms work.
+I have used d3.js framework for these animations. 
+
+### Project Structure
+
+The animation code can be found at `js/graph.js` and the sorting Algorithms can be found at `js/sortingAlgorithm.js`.
+
+
--- js/graph.js
+++ js/graph.js
@@ -2,20 +2,20 @@
 const svg = d3.select('.canvas')
     .append('svg')
         .attr('width',1100)
-        .attr('height',600);
+        .attr('height',500);
 
 //Create margins and dimensions
 const margin = {top:20, bottom:20, right:20, left:20};
 const graphWidth = 1100 - margin.left - margin.right;
-const graphHeight = 600 - margin.top - margin.bottom;
+const graphHeight = 500 - margin.top - margin.bottom;
 
-const PRIMARY_COLOR = 'blue';
+const PRIMARY_COLOR = 'yellow';
 
 // This is the color of array bars that are being compared throughout the animations.
-const SECONDARY_COLOR = 'red';
+const SECONDARY_COLOR = 'black';
 
 //let ANIMATION_SPEED_MS = document.getElementById('speed').value;
-console.log(document.getElementById('speed').value);
+//console.log(document.getElementById('speed').value);
 
 const graph = svg.append('g')
     .attr('width', graphWidth)
@@ -44,7 +44,7 @@ const update = (data) => {
         .attr('class','array-bar')
         .attr('id',(d,i) => "bar" + i)
         .attr('height', (d,i) => graphHeight - y(d))
-        .attr('fill', 'blue')
+        .attr('fill', 'yellow')
         .attr('x', (d,i) => x(i))
         .attr('y', (d,i) => y(d));
 
@@ -55,12 +55,11 @@ const update = (data) => {
             .attr('id',(d,i) => "bar" + i)
             .attr('width',x.bandwidth)
             .attr('height', (d,i) => graphHeight - y(d))
-            .attr('fill', 'blue')
+            .attr('fill', 'yellow')
             .attr('x', (d,i) => x(i))
             .attr('y', (d,i) => y(d));
     
 }
-
 function mergeAlgo(){
   const ANIMATION_SPEED_MS = document.getElementById('speed').value;
   const y = d3.scaleLinear()
@@ -68,8 +67,8 @@ function mergeAlgo(){
     .range([graphHeight,0]);
 
     const animations = getMergeSortAnimations(array);
-    console.log(animations);
-    console.log(array);
+    //console.log(animations);
+    //console.log(array);
     for (let i = 0; i < animations.length; i++) {
       //const arrayBars = document.getElementsByClassName('array-bar');
       const isColorChange = i % 3 !== 2;
@@ -77,35 +76,41 @@ function mergeAlgo(){
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOne = d3.select("#bar"+barOneIdx);
         const barTwo = d3.select("#bar"+barTwoIdx);
-        //console.log(barOne);
+        ////console.log(barOne);
         const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-        d3.timeout(function() {
+        window.timer = d3.timeout(function() {
           barOne.transition().duration(ANIMATION_SPEED_MS)
           .attr('fill',color);
         barTwo.transition().duration(ANIMATION_SPEED_MS)
           .attr('fill',color);
+        //beep();
         },i*ANIMATION_SPEED_MS);
       }
       else{
-        //console.log(i);
-        d3.timeout(function(){
+        ////console.log(i);
+        window.timer1 = d3.timeout(function(){
           const [barOneIdx, newHeight] = animations[i];
           const barOne = d3.select("#bar"+barOneIdx);
           barOne.transition().duration(ANIMATION_SPEED_MS)
             .attr('height',graphHeight - y(newHeight))
             .attr('y', y(newHeight))
             .attr('fill',PRIMARY_COLOR);
+          //beep();
         },i*ANIMATION_SPEED_MS);
       }
     }
-    d3.timeout(function(){
+    let flag = 0;
+
+    window.timer2 = d3.timeout(function(){
       for(let i=0;i<array.length;i++){
         d3.timeout(function(){
           const barOne = d3.select("#bar"+i);
           barOne.transition().duration(5)
-          .attr('fill','green');
+          .attr('fill','white');
         },i*5);
+        flag = 1;
       }
+      enableButton(myBtn);
     },animations.length*ANIMATION_SPEED_MS);
 }
 
@@ -116,8 +121,8 @@ function bubbleAlgo(){
     .range([graphHeight,0]);
 
     const animations = getBubbleSortAnimations(array);
-    console.log(animations);
-    console.log(array);
+    //console.log(animations);
+    //console.log(array);
     for (let i = 0; i < animations.length; i++) {
       //const arrayBars = document.getElementsByClassName('array-bar');
       const isColorChange = i % 4 < 2;
@@ -125,7 +130,7 @@ function bubbleAlgo(){
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOne = d3.select("#bar"+barOneIdx);
         const barTwo = d3.select("#bar"+barTwoIdx);
-        //console.log(barOne);
+        ////console.log(barOne);
         const color = i % 4 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
         d3.timeout(function() {
           barOne.transition().duration(ANIMATION_SPEED_MS)
@@ -150,9 +155,10 @@ function bubbleAlgo(){
         d3.timeout(function(){
           const barOne = d3.select("#bar"+i);
           barOne.transition().duration(5)
-          .attr('fill','green');
+          .attr('fill','white');
         },i*5);
       }
+      enableButton(myBtn);
     },animations.length*ANIMATION_SPEED_MS);
 }
 
@@ -163,8 +169,8 @@ function quickAlgo(){
     .range([graphHeight,0]);
 
     const animations = getQuickSortAnimations(array);
-    console.log(animations);
-    console.log(array);
+    //console.log(animations);
+    //console.log(array);
     for (let i = 0; i < animations.length; i++) {
       //const arrayBars = document.getElementsByClassName('array-bar');
       const isColorChange = i % 4 < 2;
@@ -172,7 +178,7 @@ function quickAlgo(){
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOne = d3.select("#bar"+barOneIdx);
         const barTwo = d3.select("#bar"+barTwoIdx);
-        //console.log(barOne);
+        ////console.log(barOne);
         const color = i % 4 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
         d3.timeout(function() {
           barOne.transition().duration(ANIMATION_SPEED_MS)
@@ -197,9 +203,10 @@ function quickAlgo(){
         d3.timeout(function(){
           const barOne = d3.select("#bar"+i);
           barOne.transition().duration(5)
-          .attr('fill','green');
+          .attr('fill','white');
         },i*5);
       }
+      enableButton(myBtn);
     },animations.length*ANIMATION_SPEED_MS);
 }
 
@@ -210,8 +217,8 @@ function insertionAlgo(){
     .range([graphHeight,0]);
 
     const animations = getInsertionSortAnimations(array);
-    console.log(animations);
-    console.log(array);
+    //console.log(animations);
+    //console.log(array);
     let flag;
     for (let i = 0; i < animations.length; i++) {
       //const arrayBars = document.getElementsByClassName('array-bar');
@@ -220,7 +227,7 @@ function insertionAlgo(){
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOne = d3.select("#bar"+barOneIdx);
         const barTwo = d3.select("#bar"+barTwoIdx);
-        //console.log(barOne);
+        ////console.log(barOne);
         const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
         d3.timeout(function() {
           barOne.transition().duration(ANIMATION_SPEED_MS)
@@ -246,9 +253,10 @@ function insertionAlgo(){
         d3.timeout(function(){
           const barOne = d3.select("#bar"+i);
           barOne.transition().duration(10)
-          .attr('fill','green');
+          .attr('fill','white');
         },i*10);
       }
+      enableButton(myBtn);
     },animations.length*ANIMATION_SPEED_MS);
 }
 
@@ -256,9 +264,6 @@ function insertionAlgo(){
 function resetArray() {
     const data = [];
     var size = document.getElementById("array_size").value;
-    //console.log(document.getElementsByName('quantity').value);
-    console.log(size);
-    //console.log(ANIMATION_SPEED_MS);
     for (let i = 0; i < size ; i++) {
       //data.push(size-i);
       data.push(randomIntFromInterval(5,750));
@@ -274,6 +279,25 @@ function generateArray(){
 }
 
 generateArray();
+
+function stopAnimation() {
+  console.log('clear timer', window.timer)
+  clearTimeout(window.timer);
+  clearTimeout(window.timer1);
+  clearTimeout(window.timer2);
+}
+
+function disableButton(btn1,btn2,btn3,btn4,btn5){
+  document.getElementById(btn1.id).disabled = true;
+  document.getElementById(btn2.id).disabled = true;
+  document.getElementById(btn3.id).disabled = true;
+  document.getElementById(btn4.id).disabled = true;
+  document.getElementById(btn5.id).disabled = true;
+}
+
+function enableButton(btn1){
+  document.getElementById(btn1.id).disabled = false;
+}
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
